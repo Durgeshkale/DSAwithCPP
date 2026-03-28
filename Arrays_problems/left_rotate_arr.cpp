beff99eb2343773ea524005092f85934f2be7633
Durgesh Kale <kaledurgesh62@gmail.com>
2026-03-28 16:31:40 +0530

    left rotate the array by one element

---
+++ Arrays_problems/left_rotate_arr.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+// This program finds the largest element in an array of integers.
+
+int main() {
+    int n;
+    cout<<"Enter te value of n :";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements of array: ";
+    for(int i=0; i<n;i++){
+        cin>>arr[i];
+    }
+    
+    int temp = arr[0];
+
+    for(int i=1; i<n; i++){
+        arr[i-1]=arr[i];
+    }
+
+    arr[n-1]=temp;
+
+    cout<<"The left rotated array is :";
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    
+
+    return 0;
+}
