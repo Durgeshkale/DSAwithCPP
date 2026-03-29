04599c93a6fd849ea54ac17628289bf04bfd687c
Durgesh Kale <kaledurgesh62@gmail.com>
2026-03-29 11:40:37 +0530

    leetcode is array sorted and rotated?

---
+++ Arrays_problems/sorted_2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
+            }
+        }
+
+        return count <= 1;
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    Solution obj;
+    cout << (obj.check(nums) ? "true" : "false") << endl;
+
+    return 0;
+}
