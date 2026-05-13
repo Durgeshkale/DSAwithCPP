0695360a7d72f7969803af003e5ecbd9b3ffe873
Durgesh Kale <kaledurgesh62@gmail.com>
2026-05-14 02:40:11 +0530

    Leetcode 152 Maximum Product subarray soln 2

--- Arrays_hard/Max_Product_Subarray.cpp
+++ Arrays_hard/Max_Product_Subarray.cpp
@@ -10,11 +10,21 @@ first appraoch is to use the prefix and suffix product
 we calculate the prefix and suffix product and take maximum of them 
 whenever we there is zero we reset the product to 1
 
+second approach 
+we can keep track of maximum and minimum product 
+we keep multiplying the current element with the max and min product both and
+so when we encounter 0 it resets
+
+and we ever we encounter it is obviouse that the opposite value will be max so we swap them and
+
+return the max product as result
+
 */
 
 #include <bits/stdc++.h>
 using namespace std;
 
+/* Solution 1
 int maxProduct(vector<int>& nums) {
     int n = nums.size();
 
@@ -36,6 +46,28 @@ int maxProduct(vector<int>& nums) {
     }
 
     return ans;
+}
+    */
+
+//Soltion 2
+
+int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        int result = nums[0];
+        int maxprod = nums[0];
+        int minprod = nums[0];
+
+        for(int i = 1;  i < n; i++){
+            int curr = nums[i];
+
+            if(curr < 0) swap(maxprod, minprod);
+
+            maxprod = max(curr, maxprod * curr);
+            minprod = min(curr, minprod * curr);
+
+            result = max(result, maxprod);
+        }
+        return result;
 }
 
 int main() {
