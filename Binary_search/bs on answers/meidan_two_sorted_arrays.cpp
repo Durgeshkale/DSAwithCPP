1c6921d3ee59fba1d1b464b17af136a09637b920
Durgesh Kale <kaledurgesh62@gmail.com>
2026-06-03 02:49:28 +0530

    Leetcode 4: Median of two sorted arrays (brute approach)

---
+++ Binary_search/bs on answers/meidan_two_sorted_arrays.cpp
@@ -0,0 +1,139 @@
+/*
+Question:
+Leetcode 4. Median of Two Sorted Arrays
+
+Given two sorted arrays nums1 and nums2 of size m and n respectively,
+return the median of the two sorted arrays.
+
+Example 1:
+Input:
+nums1 = [1,3]
+nums2 = [2]
+
+Output:
+2.0
+
+Explanation:
+Merged array = [1,2,3]
+Middle element = 2
+
+Example 2:
+Input:
+nums1 = [1,2]
+nums2 = [3,4]
+
+Output:
+2.5
+
+Explanation:
+Merged array = [1,2,3,4]
+Median = (2 + 3) / 2 = 2.5
+
+
+Brute Force Approach
+
+Intuition:
+Since both arrays are already sorted,
+we can merge them into a single sorted array.
+
+Once we get the final sorted array:
+
+If total number of elements is odd:
+Median = middle element
+
+If total number of elements is even:
+Median = (n/2 th element + (n/2 - 1) th element) / 2
+
+Approach:
+1. Merge the two sorted arrays.
+2. Find total size = n1 + n2.
+3. Check whether size is odd or even.
+4. If odd, return middle element.
+5. If even, return average of middle two elements.
+
+This is a brute force approach because:
+- We traverse both arrays completely.
+- We create an extra merged array.
+
+
+Time Complexity: O(n1 + n2)
+
+Space Complexity: O(n1 + n2)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//brute approach
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+
+    vector<int> nums3;
+
+    int n1 = nums1.size();
+    int n2 = nums2.size();
+
+    int i = 0;
+    int j = 0;
+
+    // merge both sorted arrays
+    while(i < n1 && j < n2) {
+
+        if(nums1[i] < nums2[j]) {
+            nums3.push_back(nums1[i++]);
+        }
+
+        else {
+            nums3.push_back(nums2[j++]);
+        }
+    }
+
+    // copy remaining elements of first array
+    while(i < n1) {
+        nums3.push_back(nums1[i++]);
+    }
+
+    // copy remaining elements of second array
+    while(j < n2) {
+        nums3.push_back(nums2[j++]);
+    }
+
+    int n = n1 + n2;
+
+    // odd number of elements
+    if(n % 2 == 1) {
+        return nums3[n / 2];
+    }
+
+    // even number of elements
+    return ((double)nums3[n / 2] + (double)nums3[n / 2 - 1]) / 2.0;
+}
+
+int main() {
+
+    int n1;
+    cout << "Enter size of first array: ";
+    cin >> n1;
+
+    vector<int> nums1(n1);
+
+    cout << "Enter elements of first sorted array: ";
+    for(int i = 0; i < n1; i++) {
+        cin >> nums1[i];
+    }
+
+    int n2;
+    cout << "Enter size of second array: ";
+    cin >> n2;
+
+    vector<int> nums2(n2);
+
+    cout << "Enter elements of second sorted array: ";
+    for(int i = 0; i < n2; i++) {
+        cin >> nums2[i];
+    }
+
+    cout << "Median is: ";
+    cout << findMedianSortedArrays(nums1, nums2);
+
+    return 0;
+}
