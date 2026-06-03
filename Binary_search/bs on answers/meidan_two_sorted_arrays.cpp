ef065706267b007c0cdfccad66c8363dfeb4412e
Durgesh Kale <kaledurgesh62@gmail.com>
2026-06-03 13:20:49 +0530

    Leetcode 4: Median of two sorted arrays better appraoch

--- Binary_search/bs on answers/meidan_two_sorted_arrays.cpp
+++ Binary_search/bs on answers/meidan_two_sorted_arrays.cpp
@@ -59,10 +59,7 @@ This is a brute force approach because:
 Time Complexity: O(n1 + n2)
 
 Space Complexity: O(n1 + n2)
-*/
 
-#include <bits/stdc++.h>
-using namespace std;
 
 //brute approach
 double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
@@ -106,6 +103,106 @@ double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
 
     // even number of elements
     return ((double)nums3[n / 2] + (double)nums3[n / 2 - 1]) / 2.0;
+}
+
+*/
+
+/*better approach reducing extra space
+
+Here we don't create the merged array.
+
+We only need two middle elements:
+ind1 = n/2 - 1
+ind2 = n/2
+
+So while merging, we just count the current index
+and store only those two required elements.
+
+This reduces space from O(n1 + n2) to O(1).
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+Better Approach:
+Here we don't create the merged array.
+
+We only need two middle elements:
+ind1 = n/2 - 1
+ind2 = n/2
+
+So while merging, we just count the current index
+and store only those two required elements.
+
+This reduces space from O(n1 + n2) to O(1).
+*/
+//better approach
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+
+    int n1 = nums1.size();
+    int n2 = nums2.size();
+
+    int i = 0;
+    int j = 0;
+
+    int n = n1 + n2;
+
+    int ind2 = n / 2;
+    int ind1 = ind2 - 1;
+
+    int cnt = 0;
+
+    int ind1el = -1;
+    int ind2el = -1;
+
+    while(i < n1 && j < n2) {
+
+        if(nums1[i] < nums2[j]) {
+
+            if(cnt == ind1) ind1el = nums1[i];
+            if(cnt == ind2) ind2el = nums1[i];
+
+            cnt++;
+            i++;
+        }
+
+        else {
+
+            if(cnt == ind1) ind1el = nums2[j];
+            if(cnt == ind2) ind2el = nums2[j];
+
+            cnt++;
+            j++;
+        }
+    }
+
+    while(i < n1) {
+
+        if(cnt == ind1) ind1el = nums1[i];
+        if(cnt == ind2) ind2el = nums1[i];
+
+        cnt++;
+        i++;
+    }
+
+    while(j < n2) {
+
+        if(cnt == ind1) ind1el = nums2[j];
+        if(cnt == ind2) ind2el = nums2[j];
+
+        cnt++;
+        j++;
+    }
+
+    if(n % 2 == 1) {
+        return ind2el;
+    }
+
+    return ((double)ind1el + (double)ind2el) / 2.0;
 }
 
 int main() {
